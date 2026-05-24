7edebdd096f0341136215fc604d4368f9f6294d6
jbyanx <108432086+Jbyanx@users.noreply.github.com>
2026-05-24 02:44:00 -0500

    initialize new Spring Boot project with Maven wrapper and basic structure

---
+++ src/main/java/com/unimagdalena/newtonraphsonapi/NewtonRaphsonApiApplication.java
@@ -0,0 +1,13 @@
+package com.unimagdalena.newtonraphsonapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewtonRaphsonApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewtonRaphsonApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unimagdalena/newtonraphsonapi/NewtonRaphsonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unimagdalena.newtonraphsonapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewtonRaphsonApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
